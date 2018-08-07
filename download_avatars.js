e8b970322dee7d993215a11a3eb9b928730cb19a
kanwalgupta <kanwal.gupta1@gmail.com>
2018-08-07 02:10:23 +0000

    Adding command line arguments for repo name and owner

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,17 @@
 var request = require('request');
 var password = require('./secrets.js');
+var fs = require('fs');
+
+var args = process.argv.slice(2);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-
+ if(repoOwner === undefined || repoName === undefined){
+  console.log("Please enter valid information for repo owner and repo name");
+  return ;
+ }
  var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -18,10 +24,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, JSON.parse(body));
 });
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(args[0], args[1], function(err, result) {
   console.log("Errors:", err);
   result.forEach(function(contributor){
-    console.log("avatar_url ", contributor.avatar_url);
+  downloadImageByURL(contributor.avatar_url,"avatars/"+contributor.login+ '.jpg');
   })
 
 });
+function downloadImageByURL(url, filePath) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
