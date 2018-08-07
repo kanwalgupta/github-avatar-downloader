33abf0829973720492cc2494f3fe96ba00838289
kanwalgupta <kanwal.gupta1@gmail.com>
2018-08-07 00:43:26 +0000

    List of contributors code added

--- download_avatars.js
+++ download_avatars.js
@@ -15,10 +15,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   };
   request(options, function(err, res, body) {
-    cb(err, body);
+    cb(err, JSON.parse(body));
 });
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function(contributor){
+    console.log("avatar_url ", contributor.avatar_url);
+  })
+
 });
